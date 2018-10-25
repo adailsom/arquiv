87b178181a3c924f6391a8a3712be50cdfe5606b
adailsom <44439406+adailsom@users.noreply.github.com>
2018-10-25 09:17:12 -0300

    Add files via upload

---
+++ lanche.js
@@ -0,0 +1,23 @@
+var numerico = prompt ("Digite um número");
+
+  while (numerico > 0)
+     {alert("Falta" + numerico + "segundos!");
+     numerico -- 2; },1000}
+     alert("Hora de comer cambada!!!");
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+ 
